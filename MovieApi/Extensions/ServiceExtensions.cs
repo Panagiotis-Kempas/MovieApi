0efd432b1bcf3d9178d3f6d8d8a5c3ce5931b4d4
Panos1996 <panos2171996@gmail.com>
2022-06-20 22:43:46 +0300

    Add project files.

---
+++ MovieApi/Extensions/ServiceExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Contracts;
+using LoggerService;
+
+namespace MovieApi.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+            });
+        }
+
+        public static void ConfigureIISIntergration(this IServiceCollection services)
+        {
+            services.Configure<IISOptions>(options =>
+            {
+               
+            });
+        }
+
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddSingleton<ILoggerManager, LoggerManager>();
+        }
+    }
+}
---
+++ MovieApi/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using MovieApi.Extensions;
+using NLog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
+builder.Services.AddControllers();
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntergration();
+builder.Services.ConfigureLoggerService();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
